bd0d694766cf02383a7b3b5c44eb2b9f3ee42b30
Bay <smartbay1@outlook.com>
2020-03-30 15:07:17 +0800

    last_commit result has some bug

--- angular-crash-todolist/src/app/components/todos/todos.component.ts
+++ angular-crash-todolist/src/app/components/todos/todos.component.ts
@@ -20,7 +20,16 @@ export class TodosComponent implements OnInit {
   }
 
   deleteTodo(todo:Todo) {
+    // Remove from UI
     this.todos = this.todos.filter(t => t.id !== todo.id);
+    // Remove from Server
+    this.todoService.deleteTodo(todo).subscribe();
+  }
+
+  addTodo(todo:Todo) {
+    this.todoService.addTodo(todo).subscribe(todo => {
+      this.todos.push(todo);
+    });
   }
 
 }
--- angular-crash-todolist/src/app/services/todo.service.ts
+++ angular-crash-todolist/src/app/services/todo.service.ts
@@ -25,9 +25,20 @@ export class TodoService {
     return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
 
+  // Delete Todos
+  deleteTodo(todo: Todo):Observable<Todo> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.delete<Todo>(url, httpOptions);
+  }
+
+  // Add todo
+  addTodo(todo:Todo):Observable<Todo> {
+    return this.http.post<Todo>(this.todosUrl, todo, httpOptions);
+  }
+
   // Toggle Completed
   toggleCompleted(todo: Todo):Observable<any> {
-    const url = `${this.todosUrl}/${todo.id}`
-    return this.http.put(url, todo, httpOptions)
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.put(url, todo, httpOptions);
   }
 }
